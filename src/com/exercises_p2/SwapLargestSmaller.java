f2f26c7188a6f13503630f3fbd0bc25c880e4ada
Sakreetos <nice.crack19@gmail.com>
2021-04-29 13:42:47 +0200

    2nd exercise

--- src/com/exercises_p2/SwapLargestSmaller.java
+++ src/com/exercises_p2/SwapLargestSmaller.java
@@ -3,7 +3,7 @@ package com.exercises_p2;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class MinMaxValue {
+public class SwapLargestSmaller {
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.print("Enter array length: ");
@@ -14,17 +14,25 @@ public class MinMaxValue {
             array[i] = input.nextInt();
         }
         int largestN = array[0];
+        int largestIndex = 0;
         for (int i = 0; i < arrLength; i++){
             if (largestN < array[i] ){
                 largestN = array[i];
+                largestIndex = i;
             }
         }
         int smallestN = array[0];
+        int smallestIndex = 0;
         for (int i = 0; i < arrLength; i++){
             if (smallestN > array[i] ){
                 smallestN = array[i];
+                smallestIndex = i;
             }
         }
         System.out.println("Largest number in array " + Arrays.toString(array) + " is " + largestN + " and the smallest is " + smallestN );
+        array[smallestIndex] = largestN;
+        array[largestIndex] = smallestN;
+        System.out.println("Array after swap of the largest and smaller elements: " + Arrays.toString(array));
     }
 }
+
